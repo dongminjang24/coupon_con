b55c8d8ada8424c85d7ecd58e593ae86b7ec396f
jangdm37 <jangdm37@gmail.com>
2024-06-25 02:01:35 +0900

    first coupon con

---
+++ src/test/java/io/security/couponsystem/CouponSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.security.couponsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouponSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
